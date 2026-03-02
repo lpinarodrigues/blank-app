a6871852a4896f84e6134fd8c3d822faef456359
lpinarodrigues <lpinarodrigues@gmail.com>
2026-03-02 12:42:57 +0000

    Reset de configuração para deploy limpo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,5 @@
-import subprocess
-import sys
-
-# Este arquivo serve apenas como ponte para o Streamlit Cloud
-# Ele executa o seu arquivo principal 'main.py'
-from main import *
+import streamlit as st
+from main import show_app
 
 if __name__ == "__main__":
-    import streamlit.web.cli as stcli
-    sys.argv = ["streamlit", "run", "main.py"]
-    sys.exit(stcli.main())
+    show_app()
